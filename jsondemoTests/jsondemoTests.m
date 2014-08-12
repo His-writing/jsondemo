7505b5b230faf1ea025604926c2755989516d752
shuzhenguo <shenzhenguo110@163.com>
2014-08-12 10:45:15 +0800

    Initial Commit

---
+++ jsondemoTests/jsondemoTests.m
@@ -0,0 +1,34 @@
+//
+//  jsondemoTests.m
+//  jsondemoTests
+//
+//  Created by shuzhenguo on 14-8-12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface jsondemoTests : XCTestCase
+
+@end
+
+@implementation jsondemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
